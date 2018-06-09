83699f0d3f3a94c48c45240dd8906c6fa137c33f
Nate Cheng <boo0330@gmail.com>
2018-06-09 00:49:31 -0700

    add my solutions

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
-cd solutions/
-lc-spider -c config.json
-cd ..
+lc-spider -c solutions/config.json
 npm run generate
 rm -f README.md
 cp ./solutions/README.md ./
 git add .
 git commit -m "add my solutions"
-git push origin gh-pages
+git push origin master
